130a20eaa42c1ed6a043397ab546eae5dbae9324
Brian <wabwenib66@gmail.com>
2023-07-21 20:57:09 +0300

    Updates

--- src/pages/public/ContactUs.jsx
+++ src/pages/public/ContactUs.jsx
@@ -4,12 +4,12 @@ import ContactForm from "../../components/home/contact/ContactForm";
 
 const ContactUs = () => {
   return (
-    <div className="pt-28 px-4 max-w-6xl mx-auto">
-      <div className="my-10 flex-align-center flex-wrap gap-10 max-w-7xl mx-auto px-4">
-        <div className="flex-1 basis-[25rem]">
+    <div className="pt-28 max-w-6xl w-full mx-auto">
+      <div className="my-10 sm:flex-align-center flex-wrap gap-y-10 sm:gap-10  max-w-7xl mx-auto px-4">
+        <div className="flex-1 basis-80">
           <LocationDetails />
         </div>
-        <div className="flex-1 basis-[25rem]">
+        <div className="flex-1 basis-80 mt-10 sm:m-0">
           <ContactForm />
         </div>
       </div>

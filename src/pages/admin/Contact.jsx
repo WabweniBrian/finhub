95bfe6237d75702effcd4270f9b58a807705226d
Brian <wabwenib66@gmail.com>
2023-07-22 22:24:35 +0300

    Updates

--- src/pages/admin/Contact.jsx
+++ src/pages/admin/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
 
   return (
     <div>
-      <div className="flex-center-between">
+      <div className="flex-center-center sm:flex-center-between flex-col sm:flex-row gap-y-3 sm:gap-y-0">
         <h1 className="text-2xl font-semibold">Contact STL Support</h1>
         <Breadcrumb />
       </div>
